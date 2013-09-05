f1ecf53dd8fb93767f2c5e3c3f84636aad95425e
artsoroka <vagrant@vagrant-ubuntu-precise-32.(none)>
2013-09-05 12:36:16 +0000

    Wrapper class has private field halding TCPDF instance

--- class.php
+++ class.php
@@ -2,27 +2,27 @@
 //error_reporting(false); 
 require_once('lib/tcpdf.php');
 
-class Wrapper extends TCPDF {
+class Wrapper {
 
 	public $imagesArray = array(); 
 
 	public function __construct(){
 		
-		parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$this->tcpdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-		$this->SetCreator(PDF_CREATOR);
-		$this->SetAuthor('1ADW');
-		$this->SetTitle('NWST');
-		$this->SetSubject('TEST');
-		$this->SetKeywords('KEYWORDS');
-		$this->setPrintHeader(false);
-		$this->setPrintFooter(false);
-		$this->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-		$this->SetMargins(/*PDF_MARGIN_LEFT*/ 1, /*PDF_MARGIN_TOP*/ 1, /*PDF_MARGIN_RIGHT*/ 1);
-		$this->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-		$this->setImageScale(PDF_IMAGE_SCALE_RATIO);
-		$this->SetFont('trebuchetmsi', 'BI', 20);
-		$this->AddPage();
+		$this->tcpdf->SetCreator(PDF_CREATOR);
+		$this->tcpdf->SetAuthor('1ADW');
+		$this->tcpdf->SetTitle('NWST');
+		$this->tcpdf->SetSubject('TEST');
+		$this->tcpdf->SetKeywords('KEYWORDS');
+		$this->tcpdf->setPrintHeader(false);
+		$this->tcpdf->setPrintFooter(false);
+		$this->tcpdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+		$this->tcpdf->SetMargins(/*PDF_MARGIN_LEFT*/ 1, /*PDF_MARGIN_TOP*/ 1, /*PDF_MARGIN_RIGHT*/ 1);
+		$this->tcpdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+		$this->tcpdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+		$this->tcpdf->SetFont('trebuchetmsi', 'BI', 20);
+		$this->tcpdf->AddPage();
 
 	}
 
@@ -32,32 +32,32 @@ class Wrapper extends TCPDF {
 	public function render(){
 		
 		$this->description_section(); 
-		//$this->photo_section(); 
-		$this->save(); 
-		$this->__destruct(); 
-		//print_r($this->snapshot); 
-		//$this->Output('somefile.pdf', 'I');
+		$this->photo_section(); 
+		//$this->tcpdf->save(); 
+		//$this->tcpdf->__destruct(); 
+		//print_r($this->tcpdf->snapshot); 
+		$this->tcpdf->Output('somefile.pdf', 'I');
 	}
 
 	public function save(){
-			$this->snapshot = $this; 
+			$this->tcpdf->snapshot = $this->tcpdf; 
 	}
 
 
 	public function description_section(){
 
-		$this->SetXY(0, 40);
-		$this->SetFont('trebuchetmsi', '', 16, '', true);
-		$this->SetTextColor(245,64,0);
-		$this->writeHTMLCell(0, 0, '', '', '<div style="text-align:center">HELLO<img src="logo.jpg"></div>' , 0, 1, 0, true, '', true);
+		$this->tcpdf->SetXY(0, 40);
+		$this->tcpdf->SetFont('trebuchetmsi', '', 16, '', true);
+		$this->tcpdf->SetTextColor(245,64,0);
+		$this->tcpdf->writeHTMLCell(0, 0, '', '', '<div style="text-align:center">HELLO<img src="logo.jpg"></div>' , 0, 1, 0, true, '', true);
 	}
 
 
 	public function photo_section(){
 
 		$X = 10; 
-		$Y = intval( $this->getY() ) + 1; 
-		$pages = $this->getNumPages(); 
+		$Y = intval( $this->tcpdf->getY() ) + 1; 
+		$pages = $this->tcpdf->getNumPages(); 
 
 		foreach ($this->imagesArray as $key => $value) {
 			if (  $key % 2 ) {
@@ -66,19 +66,19 @@ class Wrapper extends TCPDF {
 				$Y += 65; 
 			} else {
 
-				$this->startTransaction();
+				$this->tcpdf->startTransaction();
 				$this->draw_image($X, $Y, $value);
 
-				if($pages != $this->getNumPages()){
-					//$this->rollbackTransaction();
+				if($pages != $this->tcpdf->getNumPages()){
+					$this->tcpdf = $this->tcpdf->rollbackTransaction();
 					$Y = 0;
 
-					$this->AddPage(); 
-					$pages = $this->getNumPages(); 
+					$this->tcpdf->AddPage(); 
+					$pages = $this->tcpdf->getNumPages(); 
 
 					$this->draw_image($X, $Y, $value); 
 				} else {
-					$this->commitTransaction();
+					$this->tcpdf->commitTransaction();
 				}
 
 			}
@@ -91,8 +91,8 @@ class Wrapper extends TCPDF {
 
 	private function draw_image($x, $y, $image){
 
-	$this->SetXY($x, $y);
-	$this->Image($image, '', '', 90, 65, '', '', 'T', false, 300, '', false, false, /* Border */ false, false, false, false);
+	$this->tcpdf->SetXY($x, $y);
+	$this->tcpdf->Image($image, '', '', 90, 65, '', '', 'T', false, 300, '', false, false, /* Border */ false, false, false, false);
 
 	}
 
