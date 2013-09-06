1907a7d07a5378195f8ea8c3c0a373bcea054449
artsoroka <vagrant@vagrant-ubuntu-precise-32.(none)>
2013-09-06 11:03:05 +0000

    some more useful links

--- ed.php
+++ ed.php
@@ -7,3 +7,6 @@ http://stackoverflow.com/questions/1078729/how-to-calculate-the-height-of-a-mult
 https://sites.google.com/a/programmingusingscala.net/www/
 https://github.com/dogada/metajs
 http://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B4%D0%B6%D0%BE%D1%80%D0%BD%D0%B5%D1%80_%28%D0%BC%D0%B0%D1%80%D1%81%D0%BE%D1%85%D0%BE%D0%B4%29
+
+http://www.polymer-project.org/
+http://www.cappuccino-project.org/ 
