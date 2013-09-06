1c17641c10d101471edee40363c6a403a517ba7d
artsoroka <vagrant@vagrant-ubuntu-precise-32.(none)>
2013-09-06 09:02:49 +0000

    useful links from skype chat

--- ed.php
+++ ed.php
@@ -3,3 +3,7 @@
 echo wvi1qb83 
 
 http://stackoverflow.com/questions/1078729/how-to-calculate-the-height-of-a-multicell-writehtmlcell-in-tcpdfhttp://stackoverflow.com/questions/1078729/how-to-calculate-the-height-of-a-multicell-writehtmlcell-in-tcpdf
+
+https://sites.google.com/a/programmingusingscala.net/www/
+https://github.com/dogada/metajs
+http://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B4%D0%B6%D0%BE%D1%80%D0%BD%D0%B5%D1%80_%28%D0%BC%D0%B0%D1%80%D1%81%D0%BE%D1%85%D0%BE%D0%B4%29
